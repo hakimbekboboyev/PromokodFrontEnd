79be1803526b6f2bed55da0da842df39d527ccc6
hakimbekboboyev <hakimbekboboyev@gmail.com>
2024-08-23 16:25:49 +0500

    railway

--- src/components/PromoCard/index.jsx
+++ src/components/PromoCard/index.jsx
@@ -18,8 +18,8 @@ const PromoCard = ({ type }) => {
     axios
       .get(
         type == "all"
-          ? `http://18.220.93.119/api/v1/promo/getAllPromo`
-          : `http://18.220.93.119/api/v1/promo/category/${
+          ? `https://promokodapi-production.up.railway.app/api/v1/promo/getAllPromo`
+          : `https://promokodapi-production.up.railway.app/api/v1/promo/category/${
               location.pathname.split("/")[2]
             }`
       )
